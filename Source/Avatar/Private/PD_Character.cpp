51cc2c042d4b62ab184e1b08d702b7b44a0b91d2
sebastiansalas <sebastiansalas94@gmail.com>
2021-02-28 17:39:09 -0500

    Begin of week 2.

---
+++ Source/Avatar/Private/PD_Character.cpp
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PD_Character.h"
+
+// Sets default values
+APD_Character::APD_Character()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APD_Character::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void APD_Character::MoveForward(float value){
+    AddMovementInput(GetActorForwardVector() * value);
+}
+
+void APD_Character::MoveRight(float value){
+    AddMovementInput(GetActorRightVector() * value);
+}
+
+void APD_Character::Jump(){
+    Super::Jump();
+}
+
+void APD_Character::StopJumping(){
+    Super::StopJumping();
+}
+
+void APD_Character::AddControllerPitchInput(float value){
+    Super::AddControllerPitchInput(bIsLookInversion ? -value : value);
+}
+
+// Called every frame
+void APD_Character::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APD_Character::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+    
+    PlayerInputComponent->BindAxis("MoveForward", this, &APD_Character::MoveForward);
+    PlayerInputComponent->BindAxis("MoveRight", this, &APD_Character::MoveRight);
+    
+    PlayerInputComponent->BindAxis("LookUp", this, &APD_Character::AddControllerPitchInput);
+    PlayerInputComponent->BindAxis("LookRight", this, &ACharacter::AddControllerYawInput);
+    
+    PlayerInputComponent-> BindAction("Jump", IE_Pressed, this, &APD_Character::Jump);
+    PlayerInputComponent-> BindAction("Jump", IE_Released, this, &APD_Character::StopJumping);
+
+}
+
---
+++ Source/Avatar/Public/PD_Character.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "PD_Character.generated.h"
+
+UCLASS()
+class AVATAR_API APD_Character : public ACharacter
+{
+	GENERATED_BODY()
+
+protected:
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Aiming")
+    bool bIsLookInversion;
+    
+public:
+	// Sets default values for this character's properties
+	APD_Character();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+    void MoveForward(float value);
+    void MoveRight(float value);
+    
+    virtual void Jump() override;
+    virtual void StopJumping() override;
+    
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+    virtual void AddControllerPitchInput(float value) override;
+};
