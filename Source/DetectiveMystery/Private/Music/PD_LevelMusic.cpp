4d294e720b44457c77de3156c69c89b369fa84d2
sebastiansalas94 <sebastiansalas94@gmail.com>
2023-09-23 18:29:20 -0500

    Level Music C++ class

---
+++ Source/DetectiveMystery/Private/Music/PD_LevelMusic.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Music/PD_LevelMusic.h"
+#include "Components/BillboardComponent.h"
+#include "Components/AudioComponent.h"
+#include "Core/PD_GameMode.h"
+
+// Sets default values
+APD_LevelMusic::APD_LevelMusic()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MusicBillboardComponent = CreateDefaultSubobject<UBillboardComponent>(TEXT("MusicBillboardComponent"));
+	RootComponent = MusicBillboardComponent;
+
+	MusicAudioComponent  = CreateDefaultSubobject<UAudioComponent>(TEXT("MusicAudioComponent"));
+	MusicAudioComponent->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void APD_LevelMusic::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	GameModeReference = Cast<APD_GameMode>(GetWorld()->GetAuthGameMode());
+	if (!IsValid(GameModeReference))
+	{
+		GameModeReference->OnVictoryDelegate.AddDynamic(this, &APD_LevelMusic::StopLevelMusic);
+		GameModeReference->OnGameOverDelegate.AddDynamic(this, &APD_LevelMusic::StopLevelMusic);
+	}
+}
+
+void APD_LevelMusic::StopLevelMusic()
+{
+	MusicAudioComponent->Stop();
+}
---
+++ Source/DetectiveMystery/Public/Music/PD_LevelMusic.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PD_LevelMusic.generated.h"
+
+class UBillboardComponent;
+class UAudioComponent;
+class APD_GameMode;
+
+UCLASS()
+class DETECTIVEMYSTERY_API APD_LevelMusic : public AActor
+{
+	GENERATED_BODY()
+
+protected:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UBillboardComponent* MusicBillboardComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UAudioComponent* MusicAudioComponent;
+
+protected:
+
+	UPROPERTY(BlueprintReadOnly, Category = "References")
+	APD_GameMode* GameModeReference;
+
+public:	
+	// Sets default values for this actor's properties
+	APD_LevelMusic();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void StopLevelMusic();
+};
